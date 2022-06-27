4e7b8411c7412b3504ea14c92fc63c0ed3f9754a
Burak <burak.yilmaz@simdatix.com>
2022-06-27 18:28:15 +0300

    create UserList component

---
+++ frontend/src/user/components/UserItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UserItem.css";
+
+const UserItem = () => {
+  return <div>UserItem</div>;
+};
+
+export default UserItem;
--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "user/components/UsersList";
+
 const Users = () => {
-  return <div>Users</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Victor",
+      image:
+        "https://bestlifeonline.com/wp-content/uploads/sites/3/2020/09/man-looking-at-online-dating-profile-dog.jpg?quality=82&strip=1&resize=640%2C360",
+      places: 10,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;

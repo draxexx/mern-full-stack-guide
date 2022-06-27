98aff1ce0e7259d46859a4cdff5d151cfc6d5c4d
Burak <burak.yilmaz@simdatix.com>
2022-06-27 19:04:17 +0300

    create MainHeader, MainNavigation, NavLinks, SideDrawer components

---
+++ frontend/src/shared/components/Navigation/NavLinks.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./NavLinks.css";
+
+const NavLinks = () => {
+  return <div>NavLinks</div>;
+};
+
+export default NavLinks;
---
+++ frontend/src/shared/components/Navigation/SideDrawer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./SideDrawer.css";
+
+const SideDrawer = () => {
+  return <div>SideDrawer</div>;
+};
+
+export default SideDrawer;
